12a293f3b30ba9dac7291eb120169bf6319aa9c5
Gustavo Farias <devgustavofarias@gmail.com>
2022-01-24 11:38:43 -0300

    Update dynamodb.adapter.ts

--- src/infra/database/adapters/dynamodb.adapter.ts
+++ src/infra/database/adapters/dynamodb.adapter.ts
@@ -2,8 +2,8 @@ import dynamoose from 'dynamoose';
 
 export const dynamodbAdapter = () => {
   const db = new dynamoose.aws.sdk.DynamoDB({
-    accessKeyId: 'AKIA6A4PPU6FLYOQLTLI',
-    secretAccessKey: '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',
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: proccess.env.SECRET_KEY,
     region: 'us-east-1',
   });
 
